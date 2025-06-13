a5f43aba72d37ec81e1e4a7c3649baea91c365ba
MaxiPaz <maxipaz0047@gmail.com>
2025-06-13 10:55:03 -0300

    lectura de directorio y archivos listo

--- LectorDirectorio/Program.cs
+++ LectorDirectorio/Program.cs
@@ -1,2 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+Console.Clear();
+Console.WriteLine("\n\tLector de Directorio\n");
+Console.Write("\tIngrese el nombre del directorio: ");
+string? directorio = Console.ReadLine();
+while (string.IsNullOrEmpty(directorio) || System.IO.Directory.Exists(directorio) == false)
+{
+    if (string.IsNullOrEmpty(directorio) || System.IO.Directory.Exists(directorio) == false)
+    {
+        Console.WriteLine("\tEl nombre del directorio no puede estar vacío. Intente de nuevo.");
+        Console.Write("\n\tIngrese el nombre del directorio: ");
+        directorio = Console.ReadLine();
+
+    }
+}
+Console.WriteLine($"\n\tContenido del directorio: {directorio} \n");
+string?[] listaDirectorios = System.IO.Directory.GetDirectories(directorio);
+foreach (string? dir in listaDirectorios)
+{
+    Console.WriteLine($"\t> {dir}");
+}
+string[] nombresArchivos = System.IO.Directory.GetFiles(directorio);
+FileInfo[] listaArchivos = new FileInfo[nombresArchivos.Length];
+
+Console.WriteLine("\n\tArchivos:\n");
+for (int i = 0; i < nombresArchivos.Length; i++)
+{
+    listaArchivos[i] = new FileInfo(nombresArchivos[i]);
+    Console.WriteLine($"\t> {listaArchivos[i].Name} - {listaArchivos[i].Length} KB ");
+}
+
+
+FileInfo reporte = new(directorio + "/reporte_archivos.csv");
+using (StreamWriter sw = File.CreateText("C:/Users/maxi0/Documents/facultad/tercer_anio/taller1/practica/tl1-tp9-2025-DrMaxiPaz/LectorDirectorio"))
+{
+    sw.WriteLine($"Nombre del archivo: reporte_archivos.csv");
+    sw.WriteLine($"Tamaño del archivo:" + reporte.Length + " bytes");
+    sw.WriteLine($"Fecha de creación: {reporte.LastWriteTime}");
+}
+
+
+
+Console.WriteLine("\nFin de la lectura...\n");
